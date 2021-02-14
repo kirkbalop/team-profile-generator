d8341588f3793334636325382e980d01a702bc86
Kirk <kirkbalop@gmail.com>
2021-02-13 18:15:39 -0800

    refactored employee function to facilitate prototype syntax

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -6,3 +6,15 @@ test('initiates an Employee Object', () => {
     const emp = new Employee();
     expect(typeof(emp)).toBe("object");
 });
+
+test('can set name thru constructor arg', () => {
+    const name = 'Dave';
+    const emp = new Employee(name);
+    expect(emp.name).toBe(name);
+});
+
+test('can set id thru constructor arg', () => {
+    const idTest = 200;
+    const emp = new Employee('Dave', idTest);
+    expect(emp.id).toBe(idTest);
+});
